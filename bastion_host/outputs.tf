6e0e492a379aa5b8186dc51a2ffdb2939792517d
Gary Stewart <gary.stewart@example.com>
2019-12-21 19:21:51 +0000

    Update outputs

--- bastion_host/outputs.tf
+++ bastion_host/outputs.tf
@@ -1,3 +1,11 @@
 output "vpc_id" {
-  value = aws_vpc.bastion_vpc.id
+    value = aws_vpc.bastion_vpc.id
+}
+
+output "vpc_cidr_block" {
+    value = aws_vpc.bastion_vpc.cidr_block
+}
+
+output "vpc_route_main" {
+    value = aws_vpc.bastion_vpc.main_route_table_id
 }
