e136df62ae4a736959b26b70cc97edd07cb2b7c1
Gary Stewart <gary.stewart@example.com>
2019-12-23 14:41:18 +0000

    resources for kubernetes cluster for CKA

--- cluster/defaults.tf
+++ cluster/defaults.tf
@@ -3,3 +3,33 @@ variable "provider_vars" {
     type = "map"
     default = {}
 }
+
+variable "kube_ami_names" {
+    description = "Map of ami names for worker and master nodes"
+    type = "map"
+    default = {}
+}
+
+variable "node_count" {
+    description = "Map of node count numbers, for increasing or decreasing the amount of master and worker nodes"
+    type = "map"
+    default = {}
+}
+
+variable "common_tags" {
+    description = "Map of common tags to apply to all resources that are created with terraform"
+    type = "map"
+    default = {}
+}
+
+variable "vpc_vars" {
+    description = "map of variables that apply to our VPC we are creating for kubernetes"
+    type = "map"
+    default = {}
+}
+
+variable "cluster_name" {
+    description = "The kubernetes cluster name"
+    type = "string"
+    default = ""
+}
---
+++ cluster/kubernetes.tf
@@ -0,0 +1,11 @@
+resource "aws_vpc" "kubernetes_vpc" {
+    cidr_block             = "${var.vpc_vars["cidr_block"]}"
+    enable_dns_support     = "${var.vpc_vars["enable_dns_support"]}"
+    enable_dns_hostnames   = "${var.vpc_vars["enable_dns_hostnames"]}"
+    enable_classiclink     = "${var.vpc_vars["enable_classiclink"]}"
+    instance_tenancy       = "${var.vpc_vars["instance_tenancy"]}"
+    tags = "${merge(map(
+        "Name", "Kubernetes cluster",
+        "kubernetes.io/cluster/${var.cluster-name}", "shared"
+    ), var.common_tags)}"
+}
