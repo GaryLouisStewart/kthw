e97f5875df8aa71d0891ca3b14b5e8c22a4f876d
Gary Stewart <gary.stewart@example.com>
2020-02-29 23:50:09 +0000

    Newlines

--- cluster/backend.tf
+++ cluster/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "kubernetes-the-hard-way"
-    key    = "kthw1/kubernetesthehardway.tfstate"
+    key    = "kthw1/kubethehardway.tfstate"
     region = "eu-west-2"
   }
   required_version = "> 0.11.7"
